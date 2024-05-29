70e0ed15953c7eaf7b60fe9ed392ff264dc7347a
Dikhtiar Tania <you@example.com>
2024-05-29 22:52:34 +0300

    add new file in Formik

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,46 +1,44 @@
-import { useId, useState } from "react";
-import css from './ContactForm.module.css'
+import { useId } from "react";
+import css from "./ContactForm.module.css";
+import { Field, Form, Formik } from "formik";
 
-const initialValues = {
-    name : '',
-    phone: '',
-};
-
-const Contact = ({submit}) => {
+const Contact = ({ submit }) => {
   const uniqUserId = useId();
   const uniqNumberId = useId();
 
-  const [state, setState] = useState(initialValues);
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    submit(state) 
-    setState(initialValues)
-  }
-
-const handleChangeName = (e) => {
-    setState({
-        ...state,
-        [e.target.name]: e.target.value,
-    })
-}
-
   return (
-    <form onSubmit={handleSubmit}>
+    <Formik
+      initialValues={{
+        name: "a",
+        phone: "b",
+      }}
+      onSubmit={(values, actions) => {
+        submit(values);
+        actions.resetForm();
+      }}
+    >
+      <Form>
         <div className={css.formEl}>
-      <div className={css.formLbl}>
-        <label htmlFor={uniqUserId} className={css.formName}>Name</label>
-        <br />
-        <input id={uniqUserId} type="text" name="name" value={state.name} onChange={handleChangeName} />
-      </div>
-      <div className={css.formLbl}>
-        <label htmlFor={uniqNumberId} className={css.formName}>Number</label>
-        <br />
-        <input id={uniqNumberId} type="text" name="phone" value={state.phone} onChange={handleChangeName}/>
-      </div>
-      <button type="submit" className={css.formBtn}>Add contact</button>
-      </div>
-    </form>
+          <div className={css.formLbl}>
+            <label htmlFor={uniqUserId} className={css.formName}>
+              Name
+            </label>
+            <br />
+            <Field id={uniqUserId} type="text" name="name" />
+          </div>
+          <div className={css.formLbl}>
+            <label htmlFor={uniqNumberId} className={css.formName}>
+              Number
+            </label>
+            <br />
+            <Field id={uniqNumberId} type="text" name="phone" />
+          </div>
+          <button type="submit" className={css.formBtn}>
+            Add contact
+          </button>
+        </div>
+      </Form>
+    </Formik>
   );
 };
 
