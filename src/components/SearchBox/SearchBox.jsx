102a561463a60f6a0c4aca0b5e56835d1e86ec28
Dikhtiar Tania <you@example.com>
2024-05-29 14:13:38 +0300

    add input

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,14 +1,20 @@
-import { useId } from "react"
+import { useId, useState } from "react"
 
 const SearchBox = () => {
 
-    const uniqInputId = useId();
+const uniqInputId = useId();
+
+const [value, setValue] = useState('qwerty');
+
+const handleChange = (e) => {
+    setValue(e.target.value)
+}
 
   return (
    <form>
         <label htmlFor={uniqInputId}>Find contacts by name</label>
         <br/>
-        <input id={uniqInputId} type='text' name='userName'/>
+        <input id={uniqInputId} type='text' value={value} onChange={handleChange}/>
     </form>
   )
 }
